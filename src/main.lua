d687ed3962a1662e4273a912bc525755bafa2b02
MarcosHCK <marcoshck16@gmail.com>
2023-09-15 12:02:11 -0400

    Packing (partial)

--- src/main.lua
+++ src/main.lua
@@ -22,14 +22,65 @@ local lp = lgi.package('lpacked')
 local Lp = lgi.require('LPacked')
 
 do
+  local Gio = lgi.require ('Gio', '2.0')
+
   function lpacked.main (args)
     local app = Lp.Application.new ()
 
     function app:on_activate ()
       if (self.exec) then
         app:open ({}, 'exec')
-      else
-        error ('unimplemented')
+      elseif (self.pack) then
+        local value
+
+        do
+          local chunk
+          local env = { math = { huge = math.huge, } }
+
+          do
+            local file = Gio.File.new_for_commandline_arg (self.pack)
+            local info = assert (file:query_info ('standard::size', 0))
+            local stream = assert (file:read ())
+            local left = info:get_size ()
+
+            chunk = assert (load (function ()
+                if (left == 0) then
+                  return nil
+                else
+                  local bytes = stream:read_bytes (left)
+                  local done = bytes:get_size ()
+                    left = left - done
+                  return bytes:get_data ()
+                end
+              end, '=descriptor', 't', env))
+          end
+
+          value = not setfenv and chunk () or setfenv (chunk, env) ()
+        end
+
+        do
+          -- Check fields
+          local optional = { description = 'string', main = 'string', }
+          local mandatory = { name = 'string', pack = 'table', }
+
+          for field, type_ in ipairs (optional) do
+            local got = type (desc [field])
+            if (got ~= 'nil' and got ~= type_) then
+              local prep = type_ == 'string' and 'an' or 'a'
+              error (([[optional descriptor field '%s' must contain %s %s value]]):format(prep, type_))
+            end
+          end
+
+          for field, type_ in ipairs (mandatory) do
+            local got = type (desc [field])
+            if (got == 'nil') then
+              error (([[mandatory descriptor field '%s' is missing]]):format (field))
+            elseif (got ~= type_) then
+              local prep = type_ == 'string' and 'an' or 'a'
+              error (([[mandatory descriptor field '%s' must contain %s %s value]]):format(prep, type_))
+            end
+          end
+        end
       end
     end
 
